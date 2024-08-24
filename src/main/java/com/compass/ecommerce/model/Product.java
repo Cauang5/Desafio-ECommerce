96d66b197c3b4364f5ce187fe3faeaa92f8f4e00
Cauan Leite <cauanfc@hotmail.com>
2024-08-24 11:53:58 -0300

    feat: Complementdo as validações do Product

--- src/main/java/com/compass/ecommerce/model/Product.java
+++ src/main/java/com/compass/ecommerce/model/Product.java
@@ -1,8 +1,10 @@
 package com.compass.ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,12 +22,20 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "O nome não pode ser nulo")
+    @Size(min =  2, max = 100, message = "O nome deve conter entre 2 e 100 caracteres")
     private String name;
+
+    @NotNull(message = "A descrição não pode ser nula")
+    @Size(min = 5, max = 255, message = "A descrição deve ter entre 5 e 255 caracteres")
     private String description;
 
     @NotNull(message = "O preço não pode ser nulo ")
     @Positive(message = "O Preço deverá ser positivo")
     private BigDecimal price;
+
+    @NotNull(message = "A quantidade não pode ser nula")
+    @Min(value = 1, message = "A quantidade mínima deve ser 1")
     private Integer quantity;
 
 }
